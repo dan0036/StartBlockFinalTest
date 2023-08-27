6b316644c1b3111ed1812cb1ba9fb2f968a01592
DanM <dan36@ya.ru>
2023-08-27 14:01:45 +0300

    created income array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// create and fill preexisting three string arrays.
+string[] incomeArray = new string[3];
+for (int i = 0; i < 3; i++)
+{
+    incomeArray[i] = Console.ReadLine();
+}
+// visualising incomeArray
+Console.Write("[ " + string.Join(", ", incomeArray) + " ] -> ");
+
+int elementsQuantityOfNewArray = 0;
+
+for (int i = 0; i < incomeArray.Length; i++)
+{
+    if (incomeArray[i].Length < 4)
+    {
+        elementsQuantityOfNewArray++;
+    }
+}
+
